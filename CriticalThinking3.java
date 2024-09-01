bc5f273be5fbe1dd6f5a7970ed0c4f2734060edc
Mason F. <178356975+Mysterioso2@users.noreply.github.com>
2024-09-01 14:31:25 -0500

    Add files via upload
    week 3 files

---
+++ CriticalThinking3.java
@@ -0,0 +1,124 @@
+//Mason Fulling
+//CSU Global
+//CSC 320 - Professor C. Whitfield
+//Critical Thinking Assignment 3 - Option 1 Calculate Average Withholding
+
+
+import java.util.Scanner;
+
+public class CriticalThinking3 {
+	Scanner scnr = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		//create int for each week of income
+		//create and if-else statement with the various incomes
+		//or a switch statement
+		//then set up the tax percentages per income category
+		
+		//return the average income per each week(?)
+		//return average income per whole month as well just in case.
+		//------------------------------------------------------------------//
+		
+		double incomeWeek1;
+		double incomeWeek2;
+		double incomeWeek3;
+		double incomeWeek4;
+		//double monthlyIncome = incomeWeek1 + incomeWeek2 + incomeWeek3 + incomeWeek4;
+		double income;
+		double taxesWeek1;
+		double taxesWeek2;
+		double taxesWeek3;
+		double taxesWeek4;
+
+		//get input from the user for the income for week 1
+		//System.out.print("What is your income for week 1?");
+		//incomeWeek1 = scnr.nextInt();
+		//income = incomeWeek1;
+		//initiate switch 
+		//catch return value
+		//store return value into taxesWeek1;
+		//System.out.println("You were taxed" + taxesWeek1 + "for this week.");
+
+		
+		//get input from the user for the income for week 2
+		//System.out.print("What is your income for week 2?");
+		//incomeWeek2 = scnr.nextInt();
+		//income = incomeWeek2;
+		//initiate switch
+		//catch return value
+		//store return value into taxesWeek2;
+		//System.out.println("You were taxed" + taxesWeek2 + "for this week.");
+
+
+		//get input from the user for the income for week 3
+		//System.out.print("What is your income for week 3?");
+		//incomeWeek3 = scnr.nextInt();
+		//income = incomeWeek3;
+		//initiate switch
+		//catch return value
+		//store return value into taxesWeek3;
+		//System.out.println("You were taxed" + taxesWeek3 + "for this week.");
+
+		
+		//get input from the user for the income for week 4
+		//System.out.print("What is your income for week 4?");
+		//incomeWeek4 = scnr.nextInt();
+		//income = incomeWeek4;
+		//initiate switch
+		//catch return value
+		//store return value into taxesWeek4;
+		//System.out.println("You were taxed" + taxesWeek4 + "for this week.");
+
+
+		double monthlyIncome = incomeWeek1 + incomeWeek2 + incomeWeek3 + incomeWeek4;
+		double totalTaxes = taxesWeek1 + taxesWeek2 + taxesWeek3 + taxesWeek4;
+
+		
+		//System.out.println("You were taxed" + taxesWeek1 + "for this week.");
+
+		
+		
+		/*switch (income){
+		case 500:
+			income * 0.1 = taxesWeek1;
+			System.out.println(taxesWeek1);
+			break;
+		
+		case 1500:
+			income * 0.15 = taxesWeek2;
+			System.out.println(taxesWeek2);
+			break;
+
+		case 2500:
+			income * 0.20 = taxesWeek3;
+			System.out.println(taxesWeek4);
+			break;
+		
+		default:
+			income * 0.25 = taxesWeek3;
+			System.out.println(taxesWeek4);
+			break;
+
+		
+		return incomeTaxed
+	}
+	*/
+		
+		//Print out the average weekly tax 
+		//print out the average tax for the whole month
+		
+		
+		
+		
+		
+		
+		//if else
+		//if(weeks 1 - 4) has income less than 500 tax is 10%
+		//if else (weeks 1 - 4) has income between 500 - 1500 income tax is 15%
+		//if else (weeks 1 - 4) has income between 1500 - 2500 income tax is 20%
+		//if else (weeks 1 - 4) income is greater than 2500 income tax is 25%
+				
+		/*   **seems very wordy would have to cycle through 4 times(?)**   */
+	}
+
+}
---
+++ CriticalThinking3v2.java
@@ -0,0 +1,155 @@
+//Mason Fulling
+//CSU Global
+//CSC 320 - Professor C. Whitfield
+//Critical Thinking Assignment 3 - Option 1 Calculate Average Withholding
+
+
+import java.util.Scanner;
+
+
+public class CriticalThinking3v2 {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+
+		double incomeWeek1;
+		double incomeWeek2;
+		double incomeWeek3;
+		double incomeWeek4;
+		
+		double taxesWeek1;
+		double taxesWeek2;
+		double taxesWeek3;
+		double taxesWeek4;
+		double totalTaxes; 
+
+		System.out.print("What is your income for week 1?");
+		incomeWeek1 = scnr.nextInt();
+		if(incomeWeek1 < 500){
+			taxesWeek1 = incomeWeek1 * 0.1;
+			System.out.println("You were taxed " + taxesWeek1 + " (10%) for this week.");
+			}
+
+		else if(incomeWeek1 >= 500 && incomeWeek1 < 1500){
+			taxesWeek1 = incomeWeek1 * 0.15;
+			System.out.println("You were taxed " + taxesWeek1 + " (15%) for this week.");
+			}  
+		
+		else if(incomeWeek1 >= 1500 && incomeWeek1 < 2500){
+			taxesWeek1 = incomeWeek1 * 0.20;
+			System.out.println("You were taxed " + taxesWeek1 + " (20%) for this week.");
+			}
+		
+		else if(incomeWeek1 >= 2500){
+			taxesWeek1 = incomeWeek1 * 0.25;
+			System.out.println("You were taxed " + taxesWeek1 + " (25%) for this week.");
+			}
+		
+		else{
+			System.out.println("Invalid income.");
+			}
+
+		
+		
+		//*****WEEK 2*****
+		System.out.print("What is your income for week 2?");
+		incomeWeek2 = scnr.nextInt();
+		if(incomeWeek2 < 500){
+			taxesWeek2 = incomeWeek2 * 0.1;
+			System.out.println("You were taxed " + taxesWeek2 + " (10%) for this week.");
+			}
+			
+		else if(incomeWeek2 >= 500 && incomeWeek2 < 1500){
+			taxesWeek2 = incomeWeek2 * 0.15;
+			System.out.println("You were taxed " + taxesWeek2 + " (15%) for this week.");
+			}  
+		
+		else if(incomeWeek2 >= 1500 && incomeWeek2 < 2500){
+			taxesWeek2 = incomeWeek2 * 0.20;
+			System.out.println("You were taxed " + taxesWeek2 + " (20%) for this week.");
+			}
+		
+		else if(incomeWeek2 >= 2500){
+			taxesWeek2 = incomeWeek2 * 0.25;
+			System.out.println("You were taxed " + taxesWeek2 + " (25%) for this week.");
+			}
+		
+		else{
+			System.out.println("Invalid income.");
+			}
+
+		
+		
+		//*****WEEK 3*********
+		System.out.print("What is your income for week 3?");
+		incomeWeek3 = scnr.nextInt();
+		if(incomeWeek3 < 500){
+			taxesWeek3 = incomeWeek3 * 0.1;
+			System.out.println("You were taxed " + taxesWeek3 + " (10%) for this week.");
+			}
+			
+		else if(incomeWeek3 >= 500 && incomeWeek3 < 1500){
+			taxesWeek3 = incomeWeek3 * 0.15;
+			System.out.println("You were taxed " + taxesWeek3 + " (15%) for this week.");
+			}  
+		
+		else if(incomeWeek3 >= 1500 && incomeWeek3 < 2500){
+			taxesWeek3 = incomeWeek3 * 0.20;
+			System.out.println("You were taxed " + taxesWeek3 + " (20%) for this week.");
+			}
+		
+		else if(incomeWeek3 >= 2500){
+			taxesWeek3 = incomeWeek3 * 0.25;
+			System.out.println("You were taxed " + taxesWeek3 + " (25%) for this week.");
+			}
+		
+		else{
+			System.out.println("Invalid income.");
+			}
+		
+		
+		
+		//***** WEEK 4 ***********
+		System.out.print("What is your income for week 4?");
+		incomeWeek4 = scnr.nextInt();
+		if(incomeWeek4 < 500){
+			taxesWeek4 = incomeWeek4 * 0.1;
+			System.out.println("You were taxed " + taxesWeek4 + " (10%) for this week.");
+			}
+			
+		else if(incomeWeek4 >= 500 && incomeWeek4 < 1500){
+			taxesWeek4 = incomeWeek4 * 0.15;
+			System.out.println("You were taxed " + taxesWeek4 + " (15%) for this week.");
+			}  
+		
+		else if(incomeWeek4 >= 1500 && incomeWeek4 < 2500){
+			taxesWeek4 = incomeWeek4 * 0.20;
+			System.out.println("You were taxed " + taxesWeek4 + " (20%) for this week.");
+			}
+		
+		else if(incomeWeek4 >= 2500){
+			taxesWeek4 = incomeWeek4 * 0.25;
+			System.out.println("You were taxed " + taxesWeek4 + " (25%) for this week.");
+			}
+		
+		else{
+			System.out.println("Invalid income.");
+			}
+
+
+		
+		//monthly income		
+		double monthlyIncome = incomeWeek1 + incomeWeek2 + incomeWeek3 + incomeWeek4;
+		System.out.println("Your Total income this month was " + monthlyIncome);
+		
+		
+		//total taxes
+		//double averageTaxes = taxesWeek1 + taxesWeek2 + taxesWeek3 + taxesWeek4 / 4;
+		//System.out.println("Your average taxes per week for this month were " + averageTaxes);
+		
+		
+		
+		
+	}
+
+}

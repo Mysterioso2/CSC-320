c9b29f858edd59cdf818ddd40b17b952a65b5b17
Mason F. <178356975+Mysterioso2@users.noreply.github.com>
2024-09-30 13:05:51 -0500

    Portfolio Project Revisions - CT1: Updated Pseudocode
    I noticed that I received a “Distinguished,” grade on my pseudocode for Critical Thinking Module 1. However, I was still docked a few points for my pseudocode being sub-par, so I decided to correct that and make it a bit better. Prior to this course I had only ever really read Pseudocode and not really wrote it myself, a few simple outlines but nothing to the extent of this class. I feel as though since this assignment I have focused more on my pseudocode and have created better pseudocodes since then.

---
+++ CriticalThinking1Revision.java
@@ -0,0 +1,31 @@
+//Mason Fulling
+//CSUG: Programming 1
+//Critical Thinking Exercise #1 option2
+//August 15, 2024
+public class CriticalThinking1Revision {
+	
+
+	public static void main(String[] args) {
+
+        //initialize variables
+        String restraurantName;
+        String address;
+        String city;
+        String state;
+        int zipcode;
+
+        //pass variables with restaurant information
+        restraurantName = "Cowboy Prime";
+        address = "200 Spring Park Dr #210";
+        city = "Midland";
+        state = "TX";
+        zipcode = 79705;
+        
+        //print restaurant information
+        System.out.println("\nRestaurant Information:\n");
+		System.out.println("    " + restraurantName);
+		System.out.println("    " + address);
+		System.out.println("    " + city + ", " + state + ". " + zipcode);
+        System.out.println("\n");
+	}
+}
